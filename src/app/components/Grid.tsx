b720645b527c999474025961e3134450cf17472f
Sulit Jr, Victor <victor.sulit.jr@accenture.com>
2024-12-31 13:19:39 +0800

    add hero

---
+++ src/app/components/Grid.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Grid = () => {
+  return <section id="about"></section>;
+};
+
+export default Grid;
---
+++ src/app/components/ui/Button.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Button = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  otherClass,
+}: {
+  title: string;
+  icon: React.ReactNode;
+  position: string;
+  handleClick?: () => void;
+  otherClass?: string;
+}) => {
+  return (
+    <div>
+      <button
+        className={`w-full inline-flex h-12 animate-shimmer items-center
+       justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+       bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2
+     focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-5 gap-3 ${otherClass}`}
+      >
+        {position === "left" && icon}
+        {title}
+        {position === "right" && icon}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
